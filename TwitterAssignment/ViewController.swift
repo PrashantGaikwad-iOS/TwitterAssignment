c1ace15fc67473f89c7cf0be240da9da15d2177a
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-12-10 11:42:44 +0530

    Initial Commit

---
+++ TwitterAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterAssignment
+//
+//  Created by Prashant G on 12/10/18.
+//  Copyright © 2018 Prashant G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
